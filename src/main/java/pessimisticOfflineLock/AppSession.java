29827b5df19707416e6ef149b3a1b71d8fa487c4
jamalahmedmaaz <jamalahmedmaaz@gmail.com>
2014-06-15 23:37:37 +0530

    POL

--- src/main/java/pessimisticOfflineLock/AppSession.java
+++ src/main/java/pessimisticOfflineLock/AppSession.java
@@ -1,8 +1,18 @@
 package pessimisticOfflineLock;
 
+import optimisticOfflineLock.IdentityMap;
+
 /**
  * Created by cassandra on 6/15/14.
  */
 public class AppSession {
+    private String user;
+    private String id;
+    private IdentityMap identityMap;
 
+    public AppSession(String user, String id, IdentityMap identityMap) {
+        this.user = user;
+        this.id = id;
+        this.identityMap = identityMap;
+    }
 }
---
+++ src/main/java/pessimisticOfflineLock/AppSessionManager.java
@@ -0,0 +1,18 @@
+package pessimisticOfflineLock;
+
+/**
+ * Created by cassandra on 6/15/14.
+ */
+public class AppSessionManager {
+    private static ThreadLocal current = new ThreadLocal();
+
+    public static AppSession getSession() {
+        return (AppSession) current.get();
+    }
+
+    public static void setSession(AppSession session) {
+        current.set(session);
+    }
+
+
+}
